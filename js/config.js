f6dd16bfb711fc8e1e7c2bab0338423d59744fb8
safetybellbeen-eng <safetybellbeen@gmail.com>
2026-09-14 12:40:56 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,3 +12,4 @@ export const CONFIG = {
   SUPABASE_URL: 'https://kuphyemtyamglvyjpvwh.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_eOUPEa1xtZP5FbOoEuiWUw_2M1_Q5c5',
   KAKAO_JS_KEY: '120b5bb6ea3f8f79aa298231e1a6c04e'
+};
